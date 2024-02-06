6beef890d55e5db61cfdbadaac67363eba2f062b
EngAmel <amel.abdululazim.se@gmail.com>
2024-02-06 05:04:59 +0300

    basemodle

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,33 @@ class BaseModel:
 		any argumants and no return value "NULL"
 		"""
 		self.id = str(uuid.uuid4())
-		self.created_at =  datetime.datetime.now()""" ?? iam not soure about this line"""
+		self.created_at =  datetime.datetime.now().isoformat()
 		self.updated_at = self.created_at
 	
-	def __str__():
+	def __str__(self):
+		"""
+		this is the constructor of the base class that not take
+		any argumants and return object in dictionary representation
+		"""
+		return '[] (<>) <>'.\
+        format(type(self).__name__, self.id, self.__dict__)
+	
+	def save(self):
+		"""
+		this function  it take
+		a new value of updated_at as argumant and return nothing
+		"""
+		self.updated_at = datetime.datetime.now().isoformat()
+	
+	def to_dict(self):
+		"""
+		this function  it has no argumants and  returns 
+		a dictionary containing all keys/values of __dict__ of the instance
+		"""
+		obj_dict = self.__dict__
+		obj_dict['__class__'] = self.__class__.__name__
+		return obj_dict
+
 
 
 
