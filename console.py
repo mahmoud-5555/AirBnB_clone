cbb86e3b1d47ee52f8a717f0422f4972d64f7acd
EngAmel <amel.abdululazim.se@gmail.com>
2024-02-08 18:36:54 +0300

    update console to print newline after help commands

--- console.py
+++ console.py
@@ -4,9 +4,22 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """this simple command processor"""
-    def do_greet (self, line):
-        print("hello")
-    def do_EOF (self, line):
+    prompt = '(hbnb) '
+    def do_quit (self, line):
+        """ Quit command to exit the program"""
         return True
-if(__name__ == 'main'):
+    def do_EOF (self, line):
+        """ EOF command to exit the program"""
+        return True
+    def emptyline(self):
+        """an empty line + ENTER shouldn’t execute anything"""
+        pass
+    def do_help(self, arg):
+        """print newline after help commands"""
+        cmd.Cmd.do_help(self, arg)
+        if arg:
+            print()
+    
+if(__name__ == '__main__'):
+    """ should not be executed when imported"""
     HBNBCommand().cmdloop()
