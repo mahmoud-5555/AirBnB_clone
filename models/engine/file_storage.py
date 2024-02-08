f87b8cf91dd58c75b3f270fffe61a379d80f1276
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-02-08 04:21:36 +0200

    the last v of bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,32 +4,32 @@ this file will be response
 of convert opject to json and
 save it to file
 """
-import json
+import json,os
 
 class FileStorage:
-	"""
-	the class <FileStorage>
-	class that response of  save object to a
-	.json file.
-	"""
-	__file_path = 'file.json'
-	__objects = {}
+    """
+    the class <FileStorage>
+    class that response of  save object to a
+    .json file.
+    """
+    __file_path = 'file.json'
+    __objects = {}
 
-	def all(self):
-		"""returns the dictionary"""
-		return self.__objects
-	
-	def new(self, opj):
-		""": sets in <__objects> the <obj> with key <obj class name>.id"""
-		self.__objects[opj.id] = opj.to_dict()
+    def all(self):
+        """returns the dictionary"""
+        return self.__objects
+    
+    def new(self, opj):
+        """: sets in <__objects> the <obj> with key <obj class name>.id"""
+        self.__objects[ opj.__class__.__name__+'.'+ opj.id ] = opj.to_dict()
 
-	def save(self):
-		with open(self.__file_path,'w+', encoding='UTF-8') as json_file:
-			json.dump(self.__objects, json_file, indent=2)
-	
+    def save(self):
+        with open(self.__file_path,'w+', encoding='UTF-8') as json_file:
+            json.dump(self.__objects, json_file, indent=2)
+    
 
-	def reload(self):
-		with open(self.__file_path,'w+',encoding='UTF-8') as json_file:
-			if json_file.read():
-				print(json.load(json_file, indent=2))
-
+    def reload(self):
+        with open(self.__file_path, 'r+', encoding='UTF-8') as json_file:
+            if json_file.read():
+                json_file.seek(0)  # Move the file pointer to the beginning
+                self.__objects = json.load(json_file)
