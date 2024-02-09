3dafb496079ab641dda1806b520a7f47136f82fb
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-02-10 00:32:50 +0200

    <merge got tests>

---
+++ tests/test_BaseModel.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import unittest
+from datetime import datetime
+from ..models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_default_constructor(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj.id, str)
+        self.assertIsInstance(obj.created_at, datetime)
+        self.assertIsInstance(obj.updated_at, datetime)
+        self.assertNotEqual(obj.id, '')
+        self.assertEqual(obj.created_at, obj.updated_at)
+
+    def test_empty_kwargs(self):
+        obj = BaseModel(**{})
+        self.assertIsInstance(obj.id, str)
+        self.assertIsInstance(obj.created_at, datetime)
+        self.assertIsInstance(obj.updated_at, datetime)
+        self.assertNotEqual(obj.id, '')
+        self.assertEqual(obj.created_at, obj.updated_at)
+
+    def test_missing_attributes_in_kwargs(self):
+        obj = BaseModel(id='test_id', created_at=datetime.now().isoformat())
+        self.assertEqual(obj.id, 'test_id')
+        self.assertIsInstance(obj.created_at, datetime)
+        self.assertIsInstance(obj.updated_at, datetime)
+        self.assertEqual(obj.created_at, obj.updated_at)
+
+    def test_invalid_kwargs(self):
+        with self.assertRaises(ValueError):
+            obj = BaseModel(created_at='invalid_date_format')
+
+if __name__ == '__main__':
+    unittest.main()
