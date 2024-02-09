2b404e555374d7a5d0efe8dbb638dadec10fab67
EngAmel <amel.abdululazim.se@gmail.com>
2024-02-10 02:25:58 +0300

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,12 +3,21 @@ from models.base_model import BaseModel
 import datetime
 
 class TestBaseModel(unittest.TestCase):
-    def test___str__(self):
-        object = BaseModel()
-        object.id = "234-6787763-77"
-        object.updated_at = datetime.datetime(2017, 11, 30, 21, 5, 54, 119427)
-        object.created_at = datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)
-        str(object)
-        expected_str = "[BaseModel] (234-6787763-77){'updated_at': datetime.datetime(11, 30, 21, 5, 54, 119427), 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119427)}"
+    def test__str__(self):
+        # Create an instance of BaseModel
+        obj = BaseModel()
+        obj.id = "234-6787763-77"
+        obj.updated_at = (2017, 11, 30, 21, 5, 54, 119427)
+        obj.created_at = (2017, 9, 28, 21, 5, 54, 119427)
 
-        self.assertEqual(str(object), expected_str)
+        # Call the __str__() method
+        actual_str = str(obj)
+
+        # Define the expected string representation
+        expected_str = "[BaseModel] (234-6787763-77){'updated_at': (2017, 11, 30, 21, 5, 54, 119427), 'created_at': (2017, 9, 28, 21, 5, 54, 119427)}"
+
+        # Compare the actual and expected string representations
+        self.assertEqual(actual_str, expected_str)
+
+if __name__ == '__main__':
+    unittest.main()
